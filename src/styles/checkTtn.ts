afac697864fa8ce9dcc9dfaf5eb26e719619d520
Dmitry Moskalenko <dimamikron41@gmail.com>
2023-02-25 17:04:48 +0200

    change history height

--- src/styles/checkTtn.ts
+++ src/styles/checkTtn.ts
@@ -6,5 +6,5 @@ export const checkTtn = {
   historyVisible: 'visible transition-all opacity-100 duration-300',
   historyInvisible: 'invisible transition-all opacity-0 duration-300',
   historyWrapper:
-    'bg-slate-600 h-[250px] absolute bottom-[100px] right-0 max-[768px]:relative max-[768px]:inset-0 max-w-[250px] rounded-[6px] w-full h-full p-[10px] text-center mx-auto max-[768px]:max-w-[350px]',
+    'bg-slate-600 h-[250px] absolute bottom-[100px] right-0 max-[768px]:relative max-[768px]:inset-0 max-w-[250px] rounded-[6px] w-full max-[768px]:h-full p-[10px] text-center mx-auto max-[768px]:max-w-[350px]',
 };
